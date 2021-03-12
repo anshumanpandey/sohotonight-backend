60bfd697f54a84cade9042a81f60e4e42880a2f5
Anshuman <Anshuman>
2021-03-12 16:04:16 -0400

    FIX on route

--- src/routes/sms.route.ts
+++ src/routes/sms.route.ts
@@ -12,7 +12,7 @@ import { SmsModel, SMS_DIRECTION } from '../models/sms.model';
 
 export const postRoutes = express();
 
-postRoutes.post('/track', jwt({ secret: process.env.JWT_SECRET || 'aa', algorithms: ['HS256'] }), asyncHandler(async (req, res) => {
+postRoutes.post('/track', asyncHandler(async (req, res) => {
   console.log(req.body)
   const sms = await SmsModel.create({ body: req.body.Body, toNumber: "asdasd", fromNumber: "asd", direction: SMS_DIRECTION.INCOMING })
   res.send({ success: "Tracked" });

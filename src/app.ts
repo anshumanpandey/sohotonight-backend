5cc399262507bfc4e92277567cb50b993c5f2028
Anshuman <Anshuman>
2021-03-13 15:57:23 -0400

    FIX on file name

--- src/app.ts
+++ src/app.ts
@@ -64,7 +64,7 @@ const bootstrap = () => {
 
 if (process.env.HTTPS_ENABLED) {
     var privateKey  = fs.readFileSync(__dirname + '/../privkey.pem', 'utf8');
-    var certificate = fs.readFileSync(__dirname + '/../cert.pem', 'utf8');
+    var certificate = fs.readFileSync(__dirname + '/../cert.crt', 'utf8');
     var credentials = {key: privateKey, cert: certificate};
     var httpsServer = https.createServer(credentials, app);
 }

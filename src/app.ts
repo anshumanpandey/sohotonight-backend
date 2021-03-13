ecd742ec32eed7255aef12c336e7d70ea26b67e2
Anshuman <Anshuman>
2021-03-13 15:47:36 -0400

    HANDLEING https

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,11 @@ import * as bodyParser from 'body-parser';
 import sequelize from './utils/DB';
 import { routes } from './routes';
 import { ApiError } from './utils/ApiError';
-var morgan = require('morgan')
-var cors = require('cors')
+const morgan = require('morgan')
+const cors = require('cors')
+import * as https from "https"
+import * as http from "http"
+const fs = require('fs');
 
 const app = express();
 
@@ -22,16 +25,16 @@ app.use(bodyParser.urlencoded({
     extended: true,
 }));
 
-app.use("/authImages",express.static(join(__dirname, '..', 'authImages')));
-app.use("/banners",express.static(join(__dirname, '..', 'banners')));
-app.use("/videos",express.static(join(__dirname, '..', 'videos')));
-app.use("/profilePic",express.static(join(__dirname, '..', 'profilePic')));
-app.use("/pictures",express.static(join(__dirname, '..', 'pictures')));
+app.use("/authImages", express.static(join(__dirname, '..', 'authImages')));
+app.use("/banners", express.static(join(__dirname, '..', 'banners')));
+app.use("/videos", express.static(join(__dirname, '..', 'videos')));
+app.use("/profilePic", express.static(join(__dirname, '..', 'profilePic')));
+app.use("/pictures", express.static(join(__dirname, '..', 'pictures')));
 app.use(express.static(join(__dirname, '../templates')));
 
-app.use('/api',routes)
+app.use('/api', routes)
 
-app.use((err:any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
     if (err instanceof ApiError) {
         res.status(err.code).json({
             statusCode: err.code,
@@ -44,7 +47,7 @@ app.use((err:any, req: express.Request, res: express.Response, next: express.Nex
             statusCode: 401,
             message: "Unauthorized"
         });
-      }
+    }
     else {
         console.log(err)
         res.status(500).json({
@@ -59,7 +62,18 @@ const bootstrap = () => {
     return sequelize.authenticate()
 }
 
+if (process.env.HTTPS_ENABLED) {
+    var privateKey  = fs.readFileSync('./privkey.pem', 'utf8');
+    var certificate = fs.readFileSync('./cert.pem', 'utf8');
+    var credentials = {key: privateKey, cert: certificate};
+    var httpsServer = https.createServer(credentials, app);
+}
+
+var httpServer = http.createServer(app);
+
 export {
     app,
+    httpServer,
+    httpsServer,
     bootstrap
 };
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 require('dotenv').config()
-import {app, bootstrap} from './app'
+import {app, bootstrap, httpServer, httpsServer} from './app'
 import {AddressInfo} from 'net'
 import sequelize from './utils/DB';
 
 bootstrap()
 .then(() => sequelize.sync())
 .then(() => {
-    const server = app.listen(parseInt(process.env.PORT || '5000') || 5000, '0.0.0.0', () => {
+    const logConnection = (server: any) => () => {
         const {port, address} = server.address() as AddressInfo;
         console.log('Server listening on:','http://' + address + ':'+port);
-    });
+    }
+    const httpPort = parseInt(process.env.PORT || '5000') || 5000
+    const httpsPort = parseInt(process.env.HTTPS_PORT || '5001') || 5001
+    httpServer.listen(httpPort, logConnection(httpServer));
+    httpsServer?.listen(httpsPort, logConnection(httpsServer));
 })

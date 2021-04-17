95c417ed2661d213e85db6521c39329ed3670856
Anshuman <Anshuman>
2021-04-17 11:19:20 -0400

    FIX on message notification

--- src/models/Message.model.ts
+++ src/models/Message.model.ts
@@ -2,6 +2,7 @@ import { Table, Column, Model, DataType, HasMany, ForeignKey, BelongsTo } from '
 import UserModel from './user.model'
 import ConversationModel from './Conversation.model'
 import { sendNotificatioToUserId } from '../socketApp'
+import { ApiError } from '../utils/ApiError'
 
 export enum MESSAGES_EVENT_ENUM {
   NEW_MESSAGE = "NEW_MESSAGE"
@@ -29,7 +30,16 @@ export default class MessageModel extends Model {
 }
 
 export const createMessage = async ({ conversation, createdByUserId, body }: { conversation: ConversationModel, createdByUserId: string, body: string }) => {
-  const m = await MessageModel.create({ conversationId: conversation.id , createdByUserId, body })
-  sendNotificatioToUserId({ userId: createdByUserId == conversation.toUserId ? conversation.createdByUserId : conversation.toUserId, eventName: MESSAGES_EVENT_ENUM.NEW_MESSAGE, body: m.toJSON() })
+  const m = await MessageModel.create({ conversationId: conversation.id , createdByUserId, body }, { include: [{ model: UserModel }]})
+
+  const message = await getMessageById(m.id)
+  if (!message) throw new ApiError("Could not create messages")
+  
+  sendNotificatioToUserId({ userId: createdByUserId == conversation.toUserId ? conversation.createdByUserId : conversation.toUserId, eventName: MESSAGES_EVENT_ENUM.NEW_MESSAGE, body: message.toJSON() })
+  return m
+}
+
+const getMessageById = async (id: string) => {
+  const m = await MessageModel.findByPk(id,{ include: [{ model: UserModel }]})
   return m
 }

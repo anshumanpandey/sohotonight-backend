1afa938d2a5424cb5f25c663b8a2534425f08312
Anshuman <Anshuman>
2021-07-23 11:51:27 -0400

    FIx on props

--- src/db/migrations/20210723154258-autoincrement-id.js
+++ src/db/migrations/20210723154258-autoincrement-id.js
@@ -4,51 +4,83 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.changeColumn('AppConfigs', "id", {
       autoIncrement: true,
+      type: Sequelize.INTEGER,
+      primaryKey: true,
     })
     await queryInterface.changeColumn('AssetBoughts', "id", {
       autoIncrement: true,
+      type: Sequelize.INTEGER,
+      primaryKey: true,
     })
     await queryInterface.changeColumn('ConversationModels', "id", {
       autoIncrement: true,
+      type: Sequelize.INTEGER,
+      primaryKey: true,
     })
     await queryInterface.changeColumn('InvitationModels', "id", {
       autoIncrement: true,
+      type: Sequelize.INTEGER,
+      primaryKey: true,
     })
     await queryInterface.changeColumn('LogModels', "id", {
       autoIncrement: true,
+      type: Sequelize.INTEGER,
+      primaryKey: true,
     })
     await queryInterface.changeColumn('MessageModels', "id", {
       autoIncrement: true,
+      type: Sequelize.INTEGER,
+      primaryKey: true,
     })
     await queryInterface.changeColumn('PaymentModels', "id", {
       autoIncrement: true,
+      type: Sequelize.INTEGER,
+      primaryKey: true,
     })
     await queryInterface.changeColumn('PictureModels', "id", {
       autoIncrement: true,
+      type: Sequelize.INTEGER,
+      primaryKey: true,
     })
     await queryInterface.changeColumn('PostModels', "id", {
       autoIncrement: true,
+      type: Sequelize.INTEGER,
+      primaryKey: true,
     })
     await queryInterface.changeColumn('ServiceModels', "id", {
       autoIncrement: true,
+      type: Sequelize.INTEGER,
+      primaryKey: true,
     })
     await queryInterface.changeColumn('UserModels', "id", {
       autoIncrement: true,
+      type: Sequelize.INTEGER,
+      primaryKey: true,
     })
     await queryInterface.changeColumn('UserServiceModels', "id", {
       autoIncrement: true,
+      type: Sequelize.INTEGER,
+      primaryKey: true,
     })
     await queryInterface.changeColumn('UserServiceModels', "id", {
       autoIncrement: true,
+      type: Sequelize.INTEGER,
+      primaryKey: true,
     })
     await queryInterface.changeColumn('VideoChatModels', "id", {
       autoIncrement: true,
+      type: Sequelize.INTEGER,
+      primaryKey: true,
     })
     await queryInterface.changeColumn('VideoChatToUsers', "id", {
       autoIncrement: true,
+      type: Sequelize.INTEGER,
+      primaryKey: true,
     })
     await queryInterface.changeColumn('VideoModels', "id", {
       autoIncrement: true,
+      type: Sequelize.INTEGER,
+      primaryKey: true,
     })
   },
 

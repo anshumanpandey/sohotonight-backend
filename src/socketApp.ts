0f053e71942fa3a6417b6ea315c95f87950b96e2
APSonin <anshumanpandey71@gmail.com>
2021-09-09 14:28:44 -0400

    FIX on API

--- src/socketApp.ts
+++ src/socketApp.ts
@@ -1,90 +1,104 @@
-import * as http from "http"
-import * as socket from "socket.io"
-import * as jwtSocket from "socketio-jwt"
-import { JWT_SECRET } from "./middlewares/JwtMiddleware"
-import { DefaultEventsMap } from "socket.io/dist/typed-events"
-import InvitationModel, { INVITATION_EVENTS, doHandshake } from "./models/invitation.model"
-import * as videoCtr from "./controllers/videoChat.controller"
-import { VIDEO_CHAT_EVENTS, getOngoingVideoChats } from "./models/videoChat.model"
-import VideoModel from "./models/video.model"
-import { Logger } from "./utils/Logger"
-import MessageModel, { MESSAGES_EVENT_ENUM } from "./models/Message.model"
-import { AUTH_EVENTS } from "./controllers/auth.controller"
+import * as http from 'http';
+import * as socket from 'socket.io';
+import * as jwtSocket from 'socketio-jwt';
+import { JWT_SECRET } from './middlewares/JwtMiddleware';
+import { DefaultEventsMap } from 'socket.io/dist/typed-events';
+import InvitationModel, { INVITATION_EVENTS, doHandshake } from './models/invitation.model';
+import * as videoCtr from './controllers/videoChat.controller';
+import { VIDEO_CHAT_EVENTS, getOngoingVideoChats } from './models/videoChat.model';
+import VideoModel from './models/video.model';
+import { Logger } from './utils/Logger';
+import MessageModel, { MESSAGES_EVENT_ENUM } from './models/Message.model';
+import { AUTH_EVENTS } from './controllers/auth.controller';
 
-type SockerConnection = socket.Socket<DefaultEventsMap, DefaultEventsMap> & { decoded_token: { id: number }}
+type SockerConnection = socket.Socket<DefaultEventsMap, DefaultEventsMap> & { decoded_token: { id: number } };
 type EmitEvents = {
-    [INVITATION_EVENTS.INVITATION_ACCEPTED]: InvitationModel
-    [INVITATION_EVENTS.INVITATION_DECLINED]: InvitationModel
-    [INVITATION_EVENTS.INVITATION_CANCELLED]: InvitationModel
-    [INVITATION_EVENTS.NEW_VIDEO_INVITATION]: InvitationModel
-    [VIDEO_CHAT_EVENTS.VIDEO_CHAT_ENDED]: VideoModel,
-    [VIDEO_CHAT_EVENTS.RESUMED_VIDEO_BROADCAST]: VideoModel,
-    [VIDEO_CHAT_EVENTS.STOPPED_VIDEO_BROADCAST]: VideoModel,
-    [VIDEO_CHAT_EVENTS.RESUMED_VIDEO_AUDIO_BROADCAST]: VideoModel,
-    [VIDEO_CHAT_EVENTS.STOPPED_VIDEO_AUDIO_BROADCAST]: VideoModel,
-    [INVITATION_EVENTS.INVITATION_HANDSHAKE]: any
-    [MESSAGES_EVENT_ENUM.NEW_MESSAGE]: MessageModel
-    [AUTH_EVENTS.LOGOUT]: any
-}
-const connDictionary = new Map<string | number, socket.Socket<EmitEvents, DefaultEventsMap>>()
+  [INVITATION_EVENTS.INVITATION_ACCEPTED]: InvitationModel;
+  [INVITATION_EVENTS.INVITATION_DECLINED]: InvitationModel;
+  [INVITATION_EVENTS.INVITATION_CANCELLED]: InvitationModel;
+  [INVITATION_EVENTS.NEW_VIDEO_INVITATION]: InvitationModel;
+  [VIDEO_CHAT_EVENTS.VIDEO_CHAT_ENDED]: VideoModel;
+  [VIDEO_CHAT_EVENTS.RESUMED_VIDEO_BROADCAST]: VideoModel;
+  [VIDEO_CHAT_EVENTS.STOPPED_VIDEO_BROADCAST]: VideoModel;
+  [VIDEO_CHAT_EVENTS.RESUMED_VIDEO_AUDIO_BROADCAST]: VideoModel;
+  [VIDEO_CHAT_EVENTS.STOPPED_VIDEO_AUDIO_BROADCAST]: VideoModel;
+  [INVITATION_EVENTS.INVITATION_HANDSHAKE]: any;
+  [MESSAGES_EVENT_ENUM.NEW_MESSAGE]: MessageModel;
+  [AUTH_EVENTS.LOGOUT]: any;
+};
+const connDictionary = new Map<string | number, socket.Socket<EmitEvents, DefaultEventsMap>>();
 
-const storeUserConnection = ({ userId, socketConn }: { userId: number, socketConn: socket.Socket<DefaultEventsMap, DefaultEventsMap> }) => {
-    Logger.info(`storing connection for user ${userId}`)
-    connDictionary.set(userId, socketConn)
-}
+const storeUserConnection = ({
+  userId,
+  socketConn,
+}: {
+  userId: number;
+  socketConn: socket.Socket<DefaultEventsMap, DefaultEventsMap>;
+}) => {
+  Logger.info(`storing connection for user ${userId}`);
+  connDictionary.set(userId, socketConn);
+};
 
-export const sendNotificatioToUserId = ({ userId, eventName, body }: { userId: string | number, eventName: keyof EmitEvents, body: unknown }) => {
-    const conn = connDictionary.get(userId)
-    if (!conn) {
-        Logger.info(`connection for user ${userId} not found`)
-        return null
-    }
+export const sendNotificatioToUserId = ({
+  userId,
+  eventName,
+  body,
+}: {
+  userId: string | number;
+  eventName: keyof EmitEvents;
+  body: unknown;
+}) => {
+  const conn = connDictionary.get(userId);
+  if (!conn) {
+    Logger.info(`connection for user ${userId} not found`);
+    return null;
+  }
 
-    Logger.info(`sending event [${eventName}] to user ${userId}`)
-    conn.emit(eventName, body)
-}
+  Logger.info(`sending event [${eventName}] to user ${userId}`);
+  conn.emit(eventName, body);
+};
 
 const includeUserData = (conn: SockerConnection) => (cb: any) => {
-    return (e: any) => cb({ ...e, user: conn.decoded_token})
-}
+  return (e: any) => cb({ ...e, user: conn.decoded_token });
+};
 
-let io = null
+let io = null;
 export const startSocketServer = (s: http.Server) => {
-    io = new socket.Server(s, {
-        cors: {
-            origin: ["http://localhost:3000", "https://www.sohotonight.com", "https://sohotonight.com"],
-            methods: ["GET", "POST"]
-          }
-    })
-    io.use(jwtSocket.authorize({
-        secret: JWT_SECRET,
-        handshake: true,
-        auth_header_required: true
-    }));
+  io = new socket.Server(s, {
+    cors: {
+      origin: ['http://localhost:3000', 'https://www.sohotonight.com', 'https://sohotonight.com'],
+      methods: ['GET', 'POST'],
+    },
+  });
+  io.use(
+    jwtSocket.authorize({
+      secret: JWT_SECRET,
+      handshake: true,
+      auth_header_required: true,
+    }),
+  );
 
-    //@ts-expect-error
-    io.on('connection', (client: SockerConnection) => {
-        client.on("error", (nativeError) => {
-            console.log("socketIo error", nativeError)
-        })
-        storeUserConnection({ userId: client.decoded_token.id, socketConn: client })
-        const authWrapper = includeUserData(client)
-
-        client.on('DISCOUNT_VIDEO_CHAT', authWrapper(videoCtr.discountForVideoChat));
-        client.on('END_VIDEO_CHAT', videoCtr.onChatEnd);
-        client.on('CONNECTION_HANDSHAKE', authWrapper(doHandshake));
-        client.on('STOP_VIDEO_BROADCAST', authWrapper(videoCtr.stopVideoBroadcast));
-        client.on('RESUME_VIDEO_BROADCAST', authWrapper(videoCtr.resumeVideoBroadcast));
-        client.on('STOP_VIDEO_AUDIO_BROADCAST', authWrapper(videoCtr.stopVideoAudioBroadcast));
-        client.on('RESUME_VIDEO_AUDIO_BROADCAST', authWrapper(videoCtr.resumeVideoAudioBroadcast));
-        
-        
-        client.on('disconnect', () => {
-            console.log("disconnected")
-            getOngoingVideoChats({ relatedUser: client.decoded_token.id })
-            .then(chats => {
-                chats.map(c => videoCtr.onChatEnd(c))
-            })
-        });
+  //@ts-expect-error
+  io.on('connection', (client: SockerConnection) => {
+    client.on('error', (nativeError) => {
+      console.log('socketIo error', nativeError);
     });
-}
+    storeUserConnection({ userId: client.decoded_token.id, socketConn: client });
+    const authWrapper = includeUserData(client);
+
+    client.on('DISCOUNT_VIDEO_CHAT', authWrapper(videoCtr.discountForVideoChat));
+    client.on('END_VIDEO_CHAT', videoCtr.onChatEnd);
+    client.on('CONNECTION_HANDSHAKE', authWrapper(doHandshake));
+    client.on('STOP_VIDEO_BROADCAST', authWrapper(videoCtr.stopVideoBroadcast));
+    client.on('RESUME_VIDEO_BROADCAST', authWrapper(videoCtr.resumeVideoBroadcast));
+    client.on('STOP_VIDEO_AUDIO_BROADCAST', authWrapper(videoCtr.stopVideoAudioBroadcast));
+    client.on('RESUME_VIDEO_AUDIO_BROADCAST', authWrapper(videoCtr.resumeVideoAudioBroadcast));
+
+    client.on('disconnect', () => {
+      console.log('disconnected');
+      getOngoingVideoChats({ relatedUser: client.decoded_token.id }).then((chats) => {
+        chats.map((c) => videoCtr.onChatEnd(c));
+      });
+    });
+  });
+};
--- src/utils/AwsKinesisClient.ts
+++ src/utils/AwsKinesisClient.ts
@@ -1,84 +1,86 @@
-import AWS from "aws-sdk"
+import AWS from 'aws-sdk';
 import { Role } from 'amazon-kinesis-video-streams-webrtc';
-import { ApiError } from "./ApiError";
+import { ApiError } from './ApiError';
 
-const CHANNEL_ARN = 'arn:aws:kinesisvideo:us-east-2:678467088006:channel/soho/1631206530796'
+const CHANNEL_ARN = 'arn:aws:kinesisvideo:us-east-2:678467088006:channel/soho/1631206530796';
 
 const awsConfig = {
-    region: 'us-east-2',
-    accessKeyId: process.env.AWS_KINESIS_ACCESSKEYID,
-    secretAccessKey: process.env.AWS_KINESIS_ACCESSKEYID,
-}
+  region: 'us-east-2',
+  accessKeyId: process.env.AWS_KINESIS_ACCESSKEYID,
+  secretAccessKey: process.env.AWS_KINESIS_SECRETACCESSKEY,
+};
 
 const kinesisVideoClient = new AWS.KinesisVideo({
-    ...awsConfig,
-    correctClockSkew: true,
+  ...awsConfig,
+  correctClockSkew: true,
 });
 
 const getSiganling = async () => {
-    const getSignalingChannelEndpointResponse = await kinesisVideoClient
+  const getSignalingChannelEndpointResponse = await kinesisVideoClient
     .getSignalingChannelEndpoint({
-        ChannelARN: CHANNEL_ARN,
-        SingleMasterChannelEndpointConfiguration: {
-            Protocols: ['WSS', 'HTTPS'],
-            Role: Role.VIEWER,
-        },
-    })
-    .promise()
-
-    if (!getSignalingChannelEndpointResponse.ResourceEndpointList) {
-        throw new ApiError("Could not get SignalingChannelEndpoint")
-    }
-
-const endpointsByProtocol = getSignalingChannelEndpointResponse.ResourceEndpointList.reduce<Record<string, string>>((endpoints, endpoint) => {
-    const protocol = endpoint.Protocol;
-    if (!protocol) return endpoints
-    if (!endpoint.ResourceEndpoint) return endpoints
-
-    endpoints[protocol] = endpoint.ResourceEndpoint;
-    return endpoints;
-}, {});
-
-return endpointsByProtocol
-}
-
-const getChannels = async () => {
-    const endpointsByProtocol = await getSiganling();
-
-const kinesisVideoSignalingChannelsClient = new AWS.KinesisVideoSignalingChannels({
-    ...awsConfig,
-    endpoint: endpointsByProtocol["HTTPS"],
-    correctClockSkew: true,
-});
-
-return kinesisVideoSignalingChannelsClient
-}
-
-
-export const getIceServers = async () => {
-
-    const kinesisVideoSignalingChannelsClient = await getChannels()
-    const getIceServerConfigResponse = await kinesisVideoSignalingChannelsClient
-    .getIceServerConfig({
-        ChannelARN: CHANNEL_ARN,
+      ChannelARN: CHANNEL_ARN,
+      SingleMasterChannelEndpointConfiguration: {
+        Protocols: ['WSS', 'HTTPS'],
+        Role: Role.MASTER,
+      },
     })
     .promise();
-const iceServers: { urls: string | string[], username?: string, credential?: string}[] = [
-    { urls: `stun:stun.kinesisvideo.${awsConfig.region}.amazonaws.com:443` }
-];
-if (!getIceServerConfigResponse.IceServerList) {
-    throw new ApiError("Could not get IceServerConfig");
-}
-getIceServerConfigResponse.IceServerList.forEach(iceServer => {
-    if (!iceServer.Uris) return 
+
+  if (!getSignalingChannelEndpointResponse.ResourceEndpointList) {
+    throw new ApiError('Could not get SignalingChannelEndpoint');
+  }
+
+  const endpointsByProtocol = getSignalingChannelEndpointResponse.ResourceEndpointList.reduce<Record<string, string>>(
+    (record, endpoint) => {
+      const endpoints = { ...record };
+      const protocol = endpoint.Protocol;
+      if (!protocol) return endpoints;
+      if (!endpoint.ResourceEndpoint) return endpoints;
+
+      endpoints[protocol] = endpoint.ResourceEndpoint;
+      return endpoints;
+    },
+    {},
+  );
+
+  return endpointsByProtocol;
+};
+
+const getChannels = async () => {
+  const endpointsByProtocol = await getSiganling();
+
+  const kinesisVideoSignalingChannelsClient = new AWS.KinesisVideoSignalingChannels({
+    ...awsConfig,
+    endpoint: endpointsByProtocol.HTTPS,
+    correctClockSkew: true,
+  });
+
+  return kinesisVideoSignalingChannelsClient;
+};
+
+export type IceServerData = { urls: string | string[]; username?: string; credential?: string };
+
+export const getIceServers = async (): Promise<IceServerData[]> => {
+  const kinesisVideoSignalingChannelsClient = await getChannels();
+  const getIceServerConfigResponse = await kinesisVideoSignalingChannelsClient
+    .getIceServerConfig({
+      ChannelARN: CHANNEL_ARN,
+    })
+    .promise();
+  const iceServers: IceServerData[] = [{ urls: `stun:stun.kinesisvideo.${awsConfig.region}.amazonaws.com:443` }];
+  if (!getIceServerConfigResponse.IceServerList) {
+    throw new ApiError('Could not get IceServerConfig');
+  }
+  getIceServerConfigResponse.IceServerList.forEach((iceServer) => {
+    if (!iceServer.Uris) return;
 
     iceServers.push({
-        urls: iceServer.Uris,
-        username: iceServer.Username,
-        credential: iceServer.Password,
-    })
+      urls: iceServer.Uris,
+      username: iceServer.Username,
+      credential: iceServer.Password,
+    });
+  });
+  return iceServers;
+};
 
-});
-    return iceServers;
-
-}
+export default getIceServers;
